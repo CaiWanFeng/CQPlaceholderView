3e0ee8d49d1e0a33e638101ea549a9f2647a8bad
caiqiang <caiqiang@kuaijiankang.com>
2017-05-15 14:46:07 +0800

    Initial Commit

---
+++ CommonPlaceholderView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CommonPlaceholderView
+//
+//  Created by 蔡强 on 2017/5/15.
+//  Copyright © 2017年 kuaijiankang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CommonPlaceholderView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CommonPlaceholderView
+//
+//  Created by 蔡强 on 2017/5/15.
+//  Copyright © 2017年 kuaijiankang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
